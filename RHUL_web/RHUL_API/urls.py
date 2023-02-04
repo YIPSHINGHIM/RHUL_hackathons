5933be8d6fe5d7754d6341f439c629d63c75a734
karthick0502 <mkarthick502@gmail.com>
2023-02-04 17:27:17 +0000

    changes

--- RHUL_web/RHUL_API/urls.py
+++ RHUL_web/RHUL_API/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import Dashboard, AccountHolderAPIView
+from .views import Dashboard, AccountHolderAPIView, CompanyAPIView
 
 urlpatterns = [
     path('', Dashboard, name='/ome'),
-    path('home/', AccountHolderAPIView.as_view())
+    path('home/', AccountHolderAPIView.as_view()),
+    path('company/', CompanyAPIView.as_view())
 ]
--- RHUL_web/RHUL_API/views.py
+++ RHUL_web/RHUL_API/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from django.views.generic import DetailView
-from .models import Account_Holder
+from .models import Account_Holder, CompanyInfo
 from .serializers import Account_Holder_serializers
 from django.http import HttpResponse
 from rest_framework.response import Response
@@ -20,6 +20,15 @@ class AccountHolderAPIView(APIView):
         modeldata = Account_Holder.objects.all()
         serializer = Account_Holder_serializers(modeldata, many=True)
         return Response(serializer.data)
+        # customers = self.get_object(id)
+        # serializer = CustomerSerializer(customers)
+        # return Response(serializer.data)
+
+
+class CompanyAPIView(APIView):
+    def get(self, request):
+        model = CompanyInfo.objects.all()
+        return Response(model)
         # customers = self.get_object(id)
         # serializer = CustomerSerializer(customers)
         # return Response(serializer.data)
